f63ccff740ce0dcfbd4550c3a2428d88f2222552
alelephillips-cmyk <alelephillips@gmail.com>
2025-09-05 17:46:25 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Y6kD0KTA#CUrr8pnbyaFqh3ICrHUre_ENP_c96PuxljI3FNesQK0",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
